945e9f8af014feaba7f448db10278f71dfc63c9f
Prince_Yadav <prince31268@gmail.com>
2023-07-01 22:24:40 +0545

    Number of Inversion is solved

---
+++ Number_of_Inversion.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    int count = 0;
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            count += (mid - left + 1);
+            right++;
+        }
+    }
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+    return count;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int count = 0;
+    if (low >= high)
+        return count;
+    int mid = (low + high) / 2;
+    count += mergeSort(arr, low, mid);
+    count += mergeSort(arr, mid + 1, high);
+    count += merge(arr, low, mid, high);
+    return count;
+}
+
+int numberOfInversions(vector<int>& a, int n) {
+    return mergeSort(a, 0, n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int inversionCount = numberOfInversions(arr, n);
+    cout << "Number of inversions: " << inversionCount << endl;
+
+    return 0;
+}
